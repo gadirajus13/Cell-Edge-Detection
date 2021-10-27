1932d1e15249741d9acaf60032c383f60f121765
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2021-10-27 17:31:20 -0400

    Changed name

--- Cell Edge Detection/Test.java
+++ Cell Edge Detection/Test.java
@@ -36,6 +36,7 @@ public class Test {
     public static void main(String[] args) {
         System.getProperties().setProperty("plugins.dir", System.getProperty("user.dir")+File.separator+"dist"+File.separator);
         ImageJ ij=new ImageJ();
+        
         ij.exitWhenQuitting(true);
     }
 }
